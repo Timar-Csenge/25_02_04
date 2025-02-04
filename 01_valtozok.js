31898d1a9b3e6b54c1130cc691ddbb7e0b718cb7
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-02-04 08:57:05 +0100

    summary

---
+++ 01_valtozok.js
@@ -0,0 +1,23 @@
+console.log("sdfgdsgdgs");
+
+//változók deklarálása és használata
+var a = 2;
+console.log(a);
+
+//változó értékének a módosítása
+
+let b = 3;
+b = 4;
+console.log (b);
+const c = 5;
+console.log (c);
+//c = 6; //hiba, constanst nem lehet átírni
+
+
+//var változók újradefiniálása
+var x = 5; 
+console.log(x); 
+var x = "alma"; 
+console.log(x); 
+
+let d = 3;

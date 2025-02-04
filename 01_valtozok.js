7e6d94f88e30b2da47c2d20db3bae1158b1c6baa
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-02-04 09:34:07 +0100

    Update 01_valtozok.js

--- 01_valtozok.js
+++ 01_valtozok.js
@@ -25,3 +25,10 @@ console.log(d);
 //let d = 4; //Identifier 'd' has already been declared (at 01_valtozok.js:25:5)
 //console.log(d);
 
+let e = 2;
+console.log('a=' + e + ' típusa: ' + typeof e);
+const pi = 3.14;
+console.log('pi=' + pi + ' típusa: ' + typeof pi);
+
+console.log(0.1+0.2);
+console.log((0.1+0.2).toFixed(2));

9966bab91d9b77845dda4b38969a401c6f97dcf6
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-02-18 09:52:20 +0100

    summary

--- 03_operatorok.js
+++ 03_operatorok.js
@@ -72,10 +72,3 @@ function myFunction()
 console.log(myFunction())
 //console.log(valtozo)
 console.log(valtozo2)
-
-
-"dog" + "house" == "doghouse"; // összekapcsolta a két karakterláncot
-"dog" + 4 == "dog4"; // a számot átalakította
-4 + "4" == "44"; // karakterlánccá
-4 + 4 == 8; // összeadta a két számot
-23 - "17" == 6; // a karakterláncot átalakította számmá

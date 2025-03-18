705ea75d184a76fe36ea4e675b120d98d5a229ad
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-03-18 10:45:07 +0100

    Update 09_feladat.js

--- 09_feladat.js
+++ 09_feladat.js
@@ -1,62 +1,14 @@
-/*for (let i = 1; i <= 5; i++) {
-    console.log(i);
-    }
-    
-
-    let person = {name:"Pista", age:40, email:"pista@email.com"};
-    for (key in person) {
-    console.log(key + " = " + person[key]);
-    }
-
-
-let cars = ["BMW", "Volkswagen", "Trabant"];
-for (car in cars) {
-console.log(cars[car]);
-}
-
-
-for (car of cars) {
-console.log(car);
-}
-
-
-let i = 1;
-while (i <= 5) {
-console.log(i);
-i++
-}
-
-do {
-console.log(i);
-i++;
-} while (i <= 5);
-
-while (true) {
-    if (i > 5) {
-    break;
-    }
-    console.log(i);
-    i++;
-    }
-
-
-    while (i < 10) {
-        i++;
-        if (i % 2 == 1) {
-        continue;
-        }
-        console.log(i);
-        }
-
-//7.7 feladatok
-
-
-//1
+/*/1
 for (let i = 1; i <= 20; i++) {
     console.log(i + "," + Math.sqrt(i));
-    }*/
+    }
+*/
+//2
+for (let i = 1; i < 10; i++) {
+    console.log("🧸");
+    }
 
-//3
+/*/3
 for (let i = 1; i < 99; i++) {
     if (i % 3 == 0) {
     console.log(i);
@@ -64,3 +16,11 @@ for (let i = 1; i < 99; i++) {
 }
 
 //4
+for (let i = 50; i < 101; i++) {
+    if (i % 5 == 0) {
+    console.log(i*2);
+    }
+}
+
+//5
+console.log(Math.E);*/
